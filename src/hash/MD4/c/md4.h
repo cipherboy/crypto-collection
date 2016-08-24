d0a352bd6d421dd190dfdba7aaaf3d6e83491aca
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-08-24 15:43:56 -0500

    Added md4 core function

--- src/hash/MD4/c/md4.h
+++ src/hash/MD4/c/md4.h
@@ -38,6 +38,75 @@ static inline uint32_t cc_md4_rotl32(uint32_t data, uint32_t count) {
 	return ((data << count) | (data >> (32 - count)));
 }
 
+static inline void cc_md4_core(cc_md4* m, uint32_t block[16]) {
+	uint32_t s[4];
+	// Duplicate state
+	s[0] = m->s[0];
+	s[1] = m->s[1];
+	s[2] = m->s[2];
+	s[3] = m->s[3];
+
+	// Round 1
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_f(s[1], s[2], s[3])+block[0], 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_f(s[0], s[1], s[2])+block[1], 7);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_f(s[3], s[0], s[1])+block[2], 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_f(s[2], s[3], s[0])+block[3], 19);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_f(s[1], s[2], s[3])+block[4], 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_f(s[0], s[1], s[2])+block[5], 7);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_f(s[3], s[0], s[1])+block[6], 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_f(s[2], s[3], s[0])+block[7], 19);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_f(s[1], s[2], s[3])+block[8], 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_f(s[0], s[1], s[2])+block[9], 7);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_f(s[3], s[0], s[1])+block[10], 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_f(s[2], s[3], s[0])+block[11], 19);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_f(s[1], s[2], s[3])+block[12], 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_f(s[0], s[1], s[2])+block[13], 7);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_f(s[3], s[0], s[1])+block[14], 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_f(s[2], s[3], s[0])+block[15], 19);
+
+	// Round 2
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_g(s[1], s[2], s[3])+block[0]+0x5A827999, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_g(s[0], s[1], s[2])+block[4]+0x5A827999, 5);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_g(s[3], s[0], s[1])+block[8]+0x5A827999, 9);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_g(s[2], s[3], s[0])+block[12]+0x5A827999, 13);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_g(s[1], s[2], s[3])+block[1]+0x5A827999, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_g(s[0], s[1], s[2])+block[5]+0x5A827999, 5);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_g(s[3], s[0], s[1])+block[9]+0x5A827999, 9);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_g(s[2], s[3], s[0])+block[13]+0x5A827999, 13);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_g(s[1], s[2], s[3])+block[2]+0x5A827999, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_g(s[0], s[1], s[2])+block[6]+0x5A827999, 5);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_g(s[3], s[0], s[1])+block[10]+0x5A827999, 9);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_g(s[2], s[3], s[0])+block[14]+0x5A827999, 13);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_g(s[1], s[2], s[3])+block[3]+0x5A827999, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_g(s[0], s[1], s[2])+block[7]+0x5A827999, 5);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_g(s[3], s[0], s[1])+block[11]+0x5A827999, 9);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_g(s[2], s[3], s[0])+block[15]+0x5A827999, 13);
+
+	// Round 3
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_h(s[1], s[2], s[3])+block[0]+0x6ED9EBA1, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_h(s[0], s[1], s[2])+block[8]+0x6ED9EBA1, 9);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_h(s[3], s[0], s[1])+block[4]+0x6ED9EBA1, 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_h(s[2], s[3], s[0])+block[12]+0x6ED9EBA1, 15);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_h(s[1], s[2], s[3])+block[2]+0x6ED9EBA1, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_h(s[0], s[1], s[2])+block[10]+0x6ED9EBA1, 9);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_h(s[3], s[0], s[1])+block[6]+0x6ED9EBA1, 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_h(s[2], s[3], s[0])+block[14]+0x6ED9EBA1, 15);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_h(s[1], s[2], s[3])+block[1]+0x6ED9EBA1, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_h(s[0], s[1], s[2])+block[9]+0x6ED9EBA1, 9);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_h(s[3], s[0], s[1])+block[5]+0x6ED9EBA1, 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_h(s[2], s[3], s[0])+block[13]+0x6ED9EBA1, 15);
+	s[0] = cc_md4_rotl32(s[0]+cc_md4_h(s[1], s[2], s[3])+block[3]+0x6ED9EBA1, 3);
+	s[3] = cc_md4_rotl32(s[3]+cc_md4_h(s[0], s[1], s[2])+block[11]+0x6ED9EBA1, 9);
+	s[2] = cc_md4_rotl32(s[2]+cc_md4_h(s[3], s[0], s[1])+block[7]+0x6ED9EBA1, 11);
+	s[1] = cc_md4_rotl32(s[1]+cc_md4_h(s[2], s[3], s[0])+block[15]+0x6ED9EBA1, 15);
+
+	// Add back to md4 state.
+	m->s[0] += s[0];
+	m->s[1] += s[1];
+	m->s[2] += s[2];
+	m->s[3] += s[3];
+}
+
 extern inline void cc_md4_init(cc_md4* m) {
 	int i = 0;
 	for (i = 0; i < 16; i++) {
@@ -53,13 +122,14 @@ extern inline void cc_md4_init(cc_md4* m) {
 	m->p_len = 0;
 }
 
-extern inline void cc_md4_update(cc_md4* m, unsigned char* msg, uint64_t len) {
+extern inline void cc_md4_update(cc_md4* m, char* msg, uint64_t len) {
+
 }
 
 extern inline void cc_md4_finalize(cc_md4* m) {
 }
 
-extern inline uint8_t* cc_md4_sum(cc_md4* m, unsigned char* msg) {
+extern inline uint8_t* cc_md4_sum(cc_md4* m, char* msg) {
 	cc_md4_init(m);
 	cc_md4_update(m, msg, strlen(msg));
 	cc_md4_finalize(m);
