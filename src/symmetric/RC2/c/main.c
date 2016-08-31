c27ce1b4c3159b6e8c3c4132dd7d4e60ed9e5e6c
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-08-31 14:26:19 -0500

    Initial commit of encryption code

--- src/symmetric/RC2/c/main.c
+++ src/symmetric/RC2/c/main.c
@@ -9,5 +9,11 @@
 
 int main()
 {
+    struct rc2 r;
+    uint8_t key[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint16_t plaintext[4] = {0x0000, 0x0000, 0x0000, 0x0000};
+    rc2_init(&r, (uint8_t*) key, 8, 63);
+    rc2_encrypt(&r, (uint16_t*)  plaintext);
+    printf("\n%04x%04x,%04x%04x\n", r.R[0], r.R[1], r.R[2], r.R[3]);
     return 0;
 }
