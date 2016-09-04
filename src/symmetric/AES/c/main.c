68ebc035a95134a0d88a83d4ed5f02922added05
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-09-04 11:32:22 -0500

    AES 192-bit/256-bit encryption

--- src/symmetric/AES/c/main.c
+++ src/symmetric/AES/c/main.c
@@ -44,7 +44,7 @@ void test_aes128_encrypt()
     for (size_t i = 0; i < 16; i++) {
         printf("%02x", ciphertext[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 
 void test_aes192_key_expansion()
@@ -58,6 +58,29 @@ void test_aes192_key_expansion()
         printf("actual   [%zu]: %04x\n", i, a.skey[i]);
         //printf("0x%04x,", a.skey[i]);
         printf("expected [%zu]: %04x\n\n", i, expected[i]);
+    }
+    printf("\n\n");
+}
+
+void test_aes192_encrypt()
+{
+    struct aes192 a;
+    uint8_t plaintext[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    uint8_t ciphertext[16] = {0xdd, 0xa9, 0x7c, 0xa4, 0x86, 0x4c, 0xdf, 0xe0, 0x6e, 0xaf, 0x70, 0xa0, 0xec, 0x0d, 0x71, 0x91};
+    uint8_t key[24] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
+
+    aes192_init(&a, key);
+    aes192_encrypt(&a, plaintext);
+
+    printf("Actual:   ");
+    for (size_t i = 0; i < 16; i++) {
+        printf("%02x", a.block[i]);
+    }
+    printf("\n");
+
+    printf("Expected: ");
+    for (size_t i = 0; i < 16; i++) {
+        printf("%02x", ciphertext[i]);
     }
     printf("\n\n");
 }
@@ -76,6 +99,29 @@ void test_aes256_key_expansion()
     printf("\n\n");
 }
 
+void test_aes256_encrypt()
+{
+    struct aes256 a;
+    uint8_t plaintext[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    uint8_t ciphertext[16] = {0x8e, 0xa2, 0xb7, 0xca, 0x51, 0x67, 0x45, 0xbf, 0xea, 0xfc, 0x49, 0x90, 0x4b, 0x49, 0x60, 0x89};
+    uint8_t key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+
+    aes256_init(&a, key);
+    aes256_encrypt(&a, plaintext);
+
+    printf("Actual:   ");
+    for (size_t i = 0; i < 16; i++) {
+        printf("%02x", a.block[i]);
+    }
+    printf("\n");
+
+    printf("Expected: ");
+    for (size_t i = 0; i < 16; i++) {
+        printf("%02x", ciphertext[i]);
+    }
+    printf("\n\n");
+}
+
 int main()
 {
     printf("Expanding 128-bit key: \n");
@@ -89,6 +135,12 @@ int main()
 
     printf("Testing 128-bit encryption: \n");
     test_aes128_encrypt();
+
+    printf("Testing 192-bit encryption: \n");
+    test_aes192_encrypt();
+
+    printf("Testing 256-bit encryption: \n");
+    test_aes256_encrypt();
 
     return 0;
 }
